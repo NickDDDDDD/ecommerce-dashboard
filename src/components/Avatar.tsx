b8b319035d761837813ffbae90b4f8b96fd28105
NickDDDDDD <101779199+NickDDDDDD@users.noreply.github.com>
2025-08-19 01:46:42 +1000

    product management page basic layout
    .

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,9 @@
+const Avatar = () => {
+  return (
+    <div className="grid size-10 place-content-center overflow-hidden rounded-full bg-neutral-300">
+      Avatar
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+const Logo = () => {
+  return (
+    <div className="grid size-10 place-content-center bg-neutral-300">logo</div>
+  );
+};
+
+export default Logo;
